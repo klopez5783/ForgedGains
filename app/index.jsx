dc00ce660222eb69a0548cd09be2f1312c4feddc
klopez5783 <lkristopher62@gmail.com>
2025-01-22 21:23:35 -0500

    Refactor app layout to use SafeAreaView and ScrollView for improved responsiveness and structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl text-primary bold font-pblack">Forged Gains!</Text>
-      <Link href="/Home">Go To Home</Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className='bg-backGround h-full'>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className="w-full justify-start items-center h-full px-4 mt-4">
+           <Image source={images.logo}
+           className="w-[175px] h-[145px]" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
