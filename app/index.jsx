e80be83baa93cc0361a5033c5b34db3580d30a66
klopez5783 <lkristopher62@gmail.com>
2025-05-28 21:13:51 -0400

    feat: Enhance App layout for tablet support and remove installation verification

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { verifyInstallation } from 'nativewind';
-import { Image, ScrollView, Text, View } from 'react-native';
+import { Image, Platform, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomBTN from '../components/CustomBTN';
 import { images } from '../constants';
@@ -14,7 +13,7 @@ import "../global.css";
 
 
 export default function App() {
-  verifyInstallation();
+  
   const {user} = useGlobalContext();
   const router = useRouter();
   
@@ -33,22 +32,46 @@ export default function App() {
       <SafeAreaView style={{ flex: 1 }}>
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
           <View nativeID="IntroImageView" className="w-full justify-start items-center px-4 mt-5">
-            <Image source={images.logoV4}
+            {Platform.isPad ?
+             <View>
+                <Image source={images.logoV4}
+                className="w-[275px] h-[40vh]"
+                resizeMode='contain' />
+             </View>
+              :(
+              <Image source={images.logoV4}
               className="w-[175px] h-[35vh]"
               resizeMode='contain' />
+              )}
           </View>
-          <View nativeID="introTextView" className='flex-1 justify-start '>
+          {Platform.isPad ? (
+            <View nativeID="introTextView" className='flex-1 justify-start w-4/5 mx-auto'>
+            <Text nativeID="introText" className="text-3xl text-white font-bold text-center">
+              <Text className='text-darkGold'>Forged Gains:</Text> Where Strength is Crafted and Limits are Broken. 
+              Where ordinary is melted down and reforged into extraordinary. Are you ready to build the unbreakable?
+            </Text>
+            
+              <CustomBTN width={350} 
+              Title="I'm Ready."
+              handlePress={() => router.push('/(auth)/sign-up')}
+              otherStyles="mt-10 text-pbold"
+              textStyles="text-xl "
+              />
+          </View>
+          ) : (
+            <View nativeID="introTextView" className='flex-1 justify-start '>
             <Text nativeID="introText" className="text-2xl text-white font-bold text-center">
               <Text className='text-darkGold'>Forged Gains:</Text> Where Strength is Crafted and Limits are Broken. 
               Where ordinary is melted down and reforged into extraordinary. Are you ready to build the unbreakable?
             </Text>
             
-          <CustomBTN width={250} 
-          Title="I'm Ready."
-          handlePress={() => router.push('/(auth)/sign-up')}
-          otherStyles="mt-5"
-           />
+              <CustomBTN width={250} 
+              Title="I'm Ready."
+              handlePress={() => router.push('/(auth)/sign-up')}
+              otherStyles="mt-5"
+              />
           </View>
+          )}
         </ScrollView>
         <StatusBar backgroundColor="#161622" style="light" />
       </SafeAreaView>
